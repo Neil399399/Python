bf1bd87a994a96e773f2c1649069006a2e085295
neil399399 <neil399399@gmail.com>
2018-06-24 21:01:57 +0800

    predictor dome.

--- CNN/predictor.py
+++ CNN/predictor.py
@@ -1,4 +1,4 @@
-from TFRecord import get_File,float32_feature,bytes_feature
+from TFRecord import bytes_feature
 from skimage import io
 from utilities.log import TensorFlow_log
 import tensorflow as tf
@@ -12,40 +12,40 @@ one_hot_depth = 4
 
 
 if __name__ =='__main__':
-    # try:
-    #     image = io.imread("image.jpg")
-    #     if image is None:
-    #         TensorFlow_log.warning('Error images')
-    #     else:
-    #         image_raw = image.tostring()
-    #     # check the image shape.        
-    #     image_byte = bytes_feature(image_raw)
-    #     image_content = tf.decode_raw(image_byte, tf.uint8)
-    #     image = tf.reshape(image_content, [IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEPTH])
-    #     img = tf.cast(image, tf.float32) * (1. / 255) - 0.5
-    # except:
-    #     # image is not in this folder.
-    #     TensorFlow_log.error('Input prediction image failed.')
+    TensorFlow_log.info('Start predict.')
+    TensorFlow_log.info('load images.')
+    try:
+        image = io.imread("./example_data/new-7.jpg")
+        if image is None:
+            TensorFlow_log.warning('Error images')
+        else:
+            image_raw = image.tostring()
+        # check the image shape.
+        image_byte = bytes_feature(image_raw)
+        image_content = tf.decode_raw([image_raw], tf.uint8)
+        image = tf.reshape(image_content, [IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEPTH])
+        img = tf.cast(image, tf.float32) * (1. / 255) - 0.5
+    except:
+        # image is not in this folder.
+        TensorFlow_log.error('Input prediction image failed.')
       
-    
-    # setting placeholder.
-    # tf_x = tf.placeholder(tf.float32, [None, IMAGE_HEIGHT,IMAGE_WIDTH,IMAGE_DEPTH])/255
-    # image = tf.reshape(tf_x, [-1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEPTH])     # (batch, height, width, channel)
-    # tf_y = tf.placeholder(tf.float32, [None,one_hot_depth])
-    # Add ops to save and restore all the variables.
-    # saver = tf.train.Saver()
+    # load graph.
     saver = tf.train.import_meta_graph('./Model/CNN.model-199.meta')
-    # turn on tensorflow.
+    # load model.
     sess = tf.Session()
     init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+    # load input,output value.
     graph = tf.get_default_graph()
-    w1 = graph.get_tensor_by_name('tf_x:0')
-    output = graph.get_tensor_by_name('output:0')
-    # set test dict.
-    saver.restore(sess,tf.train.latest_checkpoint('./Model'))
-    print(output)
+    tf_x = graph.get_tensor_by_name('tf_x:0')
+    saver.restore(sess,tf.train.latest_checkpoint('./Model/'))
+    output = graph.get_tensor_by_name('output/MatMul:0')
 
-    # test_feature = sess.run(img)
+    # initialize input value.
+    input = sess.run([img])
+    test_output = sess.run(output, {tf_x: input})
+    prediction = np.argmax(test_output, 1)
+    print('Prediction:',prediction)
+ 
 
 
 
