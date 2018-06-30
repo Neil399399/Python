dbd5c1154d185036abcacaf4ea5d885379eea091
neil399399 <neil399399@gmail.com>
2018-07-01 01:31:17 +0800

    update learning rate

--- CNN/TFRecord.py
+++ CNN/TFRecord.py
@@ -120,6 +120,6 @@ def TFRecord_Reader(TFRecord_Files,IMAGE_HEIGHT,IMAGE_WIDTH,IMAGE_DEPTH,Batch_Si
     images, labels = tf.train.shuffle_batch(
                             [resized_image, label],
                             batch_size= Batch_Size,
-                            capacity=100+3*Batch_Size,
-                            min_after_dequeue=70)
+                            capacity=200+3*Batch_Size,
+                            min_after_dequeue=100)
     return images, labels
