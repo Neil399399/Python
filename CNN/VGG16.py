b5fb2fff3d4b5b4943b5358a1b467961e52d61ce
neil399399 <neil399399@gmail.com>
2018-07-05 14:01:53 +0800

    update VGG

--- CNN/VGG16.py
+++ CNN/VGG16.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from cnn import Vgg16
+from cnn import Vgg16,Train
 from skimage import io
 from TFRecord import get_File,TFRecord_Writer,TFRecord_Reader
 from utilities.log import TensorFlow_log
@@ -17,36 +17,28 @@ if __name__ =='__main__':
     train_images,train_labels = TFRecord_Reader('./TFRecord/train.tfrecord',IMAGE_HEIGHT,IMAGE_WIDTH,IMAGE_DEPTH,50)
     test_images,test_labels = TFRecord_Reader('./TFRecord/test.tfrecord',IMAGE_HEIGHT,IMAGE_WIDTH,IMAGE_DEPTH,30)
 
-    # sess = tf.Session()
-    # init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+    sess = tf.Session()
+    init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
 
-    # # open queue.
-    # coord = tf.train.Coordinator()
-    # threads = tf.train.start_queue_runners(sess=sess,coord=coord)
-    # sess.run(init_op)
+    # open queue.
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess,coord=coord)
+    sess.run(init_op)
 
-    # # set train dict.
-    # train_feature, train_label = sess.run([train_images,train_labels])
-    # # decode train_label to one_hot.
-    # train_label_onehot = sess.run(tf.one_hot(train_label,one_hot_depth))
+    # set train dict.
+    train_feature, train_label = sess.run([train_images,train_labels])
+    # decode train_label to one_hot.
+    train_label_onehot = sess.run(tf.one_hot(train_label,one_hot_depth))
 
-    # # set test dict.
-    # test_feature, test_label = sess.run([test_images,test_labels])
-    # # decode test_label to one_hot.
-    # test_label_onehot = sess.run(tf.one_hot(test_label,one_hot_depth))
+    # set test dict.
+    test_feature, test_label = sess.run([test_images,test_labels])
+    # decode test_label to one_hot.
+    test_label_onehot = sess.run(tf.one_hot(test_label,one_hot_depth))
 
-    vgg = Vgg16(vgg16_npy_path='./utilities/vgg16.npy',output_layer_units=6,LR=0.001)
-    train_feature,train_label_onehot,test_feature,test_label_onehot= vgg.load_data(train_images,test_images,train_labels,test_labels,6)
 
     TensorFlow_log.info('Make graph and start training.')
-    for step in range(101):
-        train_loss = vgg.train(train_feature,train_label_onehot)
-        TensorFlow_log.info('train step: %d ,loss: %s',step,train_loss)
-        if step % 10 == 0:
-           loss, accuracy, precision, recall = vgg.validate(test_feature,test_label_onehot)
-           TensorFlow_log.info('Step %d',step)
-           TensorFlow_log.info('Loss: %s , Acc: %.2f , Precision: %.2f , Recall: %.2f',loss,accuracy,precision,recall)
+    Train(train_feature,train_label_onehot,test_feature,test_label_onehot)
     # close queue.
-    # coord.request_stop()
-    # coord.join(threads)
+    coord.request_stop()
+    coord.join(threads)
     TensorFlow_log.info('CNN training done.')
